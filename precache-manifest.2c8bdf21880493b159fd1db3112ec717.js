acf905db00737dda916a82d701518806a16caf9a
peter14067 <peter14067@gmail.com>
2020-04-20 21:21:13 +0800

    Updates

---
+++ precache-manifest.2c8bdf21880493b159fd1db3112ec717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eaeaed3e8d1636c48481bd017cd4b6e",
+    "url": "/test3/index.html"
+  },
+  {
+    "revision": "e9d6558c286bbd5640a0",
+    "url": "/test3/static/css/main.98790d08.chunk.css"
+  },
+  {
+    "revision": "289199560c70e1b9dcd2",
+    "url": "/test3/static/js/2.ed9e2a01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test3/static/js/2.ed9e2a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d6558c286bbd5640a0",
+    "url": "/test3/static/js/main.b11a3012.chunk.js"
+  },
+  {
+    "revision": "2911f6b0e3fba98b1602",
+    "url": "/test3/static/js/runtime-main.cd41fb56.js"
+  }
+]);
